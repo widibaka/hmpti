5cd27f7d1c468fb9604c5cd9aa7c30139487de38
Widibaka <widibaka55@gmail.com>
2021-02-21 19:51:08 +0700

    Update db_hmpti.sql

--- db_hmpti.sql
+++ db_hmpti.sql
@@ -21,15 +21,16 @@ CREATE TABLE IF NOT EXISTS `h_carousel` (
   `image` varchar(100) DEFAULT '',
   `posisi` varchar(50) DEFAULT '',
   PRIMARY KEY (`id_carousel`)
-) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=15 DEFAULT CHARSET=utf8mb4;
 
--- Dumping data for table db_hmpti.h_carousel: ~3 rows (approximately)
+-- Dumping data for table db_hmpti.h_carousel: ~2 rows (approximately)
 DELETE FROM `h_carousel`;
 /*!40000 ALTER TABLE `h_carousel` DISABLE KEYS */;
 INSERT INTO `h_carousel` (`id_carousel`, `include_logo`, `judul`, `paragraf`, `image`, `posisi`) VALUES
 	(11, 1, 'Lungman Petrol', 'Arknight, saya gak main. Cuman sekedar tahu aja.', '1613749647.jpg', 'middle'),
 	(12, 0, 'Tentacle Aliens', 'Aquarium', '1613749696.jpg', 'start'),
-	(13, 1, 'Jokowi &amp; Putin', 'Kerja sama pembangunan tambang minyak di Tuban, Jawa Timur dengan menggandeng perusahaan petrol asal Russia.', '1613795130.JPG', 'start');
+	(13, 1, 'Jokowi &amp; Putin', 'Kerja sama pembangunan tambang minyak di Tuban, Jawa Timur dengan menggandeng perusahaan petrol asal Russia.', '1613795130.JPG', 'start'),
+	(14, 1, 'Car Crash', '', '1613863081.jpg', 'end');
 /*!40000 ALTER TABLE `h_carousel` ENABLE KEYS */;
 
 -- Dumping structure for table db_hmpti.h_detail_organisasi
@@ -61,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `h_divisi` (
   PRIMARY KEY (`id_divisi`) USING BTREE
 ) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
--- Dumping data for table db_hmpti.h_divisi: ~7 rows (approximately)
+-- Dumping data for table db_hmpti.h_divisi: ~6 rows (approximately)
 DELETE FROM `h_divisi`;
 /*!40000 ALTER TABLE `h_divisi` DISABLE KEYS */;
 INSERT INTO `h_divisi` (`id_divisi`, `nama_divisi`, `deskripsi`) VALUES
@@ -86,15 +87,16 @@ CREATE TABLE IF NOT EXISTS `h_events` (
   `poster` varchar(200) NOT NULL DEFAULT '',
   `deskripsi` text NOT NULL DEFAULT '',
   `publish` int(1) DEFAULT 0,
+  `author` varchar(500) DEFAULT '',
   PRIMARY KEY (`id_event`)
 ) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table db_hmpti.h_events: ~2 rows (approximately)
 DELETE FROM `h_events`;
 /*!40000 ALTER TABLE `h_events` DISABLE KEYS */;
-INSERT INTO `h_events` (`id_event`, `judul`, `jadwal`, `last_update`, `link_daftar`, `thumbnail`, `poster`, `deskripsi`, `publish`) VALUES
-	(1, 'Webinar Evangelion 4.0', 1613808865, 1613204083, 'http://google.com', 'thumb-1613204083.jpg', 'poster-1613204083.jpg', 'Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.', 1),
-	(2, 'Webinar Evangelion 3.0', 1613108865, 1613204083, 'http://google.com', 'thumb-1613204083.jpg', 'poster-1613204083.jpg', 'Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.', 1);
+INSERT INTO `h_events` (`id_event`, `judul`, `jadwal`, `last_update`, `link_daftar`, `thumbnail`, `poster`, `deskripsi`, `publish`, `author`) VALUES
+	(1, 'Webinar Evangelion 4.0', 1613808865, 1613204083, 'http://google.com', 'thumb-1613204083.jpg', 'poster1.jpg', '<p>Halo, di sini adalah keterangan untuk contoh event di HMP TI.&nbsp;</p><p>Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit.</p><p>Syarat dan Ketentuan:<br><span style="font-size: 1rem;">1. Bla bla bla<br></span><span style="font-size: 1rem;">2. Bla bla bla<br></span>3. Bla bla bla<br>4. Bla bla bla<br>5. Bla bla bla<br>6. Bla bla bla</p><p>Selamat bergabung, dan selamat belajar :D</p>', 1, '0'),
+	(2, 'Webinar Evangelion 3.0', 1613108865, 1613204083, 'http://google.com', 'thumb-1613204083.jpg', 'poster-1613204083.jpg', 'Honkai Impact 4.1 sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.', 1, '0');
 /*!40000 ALTER TABLE `h_events` ENABLE KEYS */;
 
 -- Dumping structure for table db_hmpti.h_jabatan
@@ -110,7 +112,7 @@ CREATE TABLE IF NOT EXISTS `h_jabatan` (
 DELETE FROM `h_jabatan`;
 /*!40000 ALTER TABLE `h_jabatan` DISABLE KEYS */;
 INSERT INTO `h_jabatan` (`id_jabatan`, `nama_jabatan`, `id_divisi`) VALUES
-	(1, 'Ketua TEST', 1),
+	(1, 'Ketua', 1),
 	(2, 'Wakil Ketua', 1),
 	(3, 'Sekretaris 1', 2),
 	(4, 'Bendahara 1', 3),
@@ -134,22 +136,69 @@ CREATE TABLE IF NOT EXISTS `h_member` (
   `aktif` int(1) DEFAULT 1,
   PRIMARY KEY (`nim`),
   UNIQUE KEY `email` (`email`)
-) ENGINE=InnoDB AUTO_INCREMENT=190103156 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=190103157 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table db_hmpti.h_member: ~9 rows (approximately)
 DELETE FROM `h_member`;
 /*!40000 ALTER TABLE `h_member` DISABLE KEYS */;
 INSERT INTO `h_member` (`nim`, `email`, `nama`, `id_jabatan`, `kontak`, `deskripsi`, `image`, `aktif`) VALUES
-	(121212122, 'widi@ggvvvvvvvvvvvvvvv', 'Dummy', 8, '', 'a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a ', '', 1),
+	(121212122, 'widi@ggvvvvvvvvvvvvvvv', 'Dummy', 8, '', 'a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a a ', '', 0),
 	(180103065, ' andreas_abi@fikom.udb.ac.id', 'Andreas Abi Permana', 1, 'Email: andreas_abi@fikom.udb.ac.id, Whatsapp: 088888888', 'Ketua Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103065.jpeg?1613381510', 1),
 	(180103066, 'f', 'Matin', 2, 'Email: matin@gmail.com', 'Ketua Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103066.jpg?1613380182', 1),
 	(180103067, 'e', 'Mbak Jami', 3, '', 'Sekretaris Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103067.jpg?1613381569', 1),
-	(180103068, 'd', 'Lupa', 4, '', 'Bendahara Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103068.jpeg?1613381587', 1),
-	(180103069, 'c', 'Mas Tomi', 5, '', 'Bendahara Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103069.jpg?1613381628', 1),
+	(180103068, 'd', 'Lupa', 0, '', 'Bendahara Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103068.jpeg?1613381587', 0),
+	(180103069, 'c', 'Mas Tomi', 5, '', 'Bendahara Donec ullamcorper nulla non metus auctor fringilla. Vestibulum id ligula porta felis euismod semper. Praesent commodo cursus magna, vel scelerisque nisl consectetur. Fusce dapibus, tellus ac cursus commodo.', '180103069.jpg?1613381628', 0),
 	(180103123, 'b', 'Yulidar', 9, '', 'Yulidar adalah seorang manusia. Manusia yang bernama Yulidar.', '180103123.png?1613404722', 1),
-	(180103159, 'widi_dwi@fikom.udb.ac.id', 'Widi Baka', 6, 'Email: hmpti@gmail.com, Whatsapp: 08XXXXXXXX', 'Aku seorang pelaut. Aku suka memancing ikan cupang.', '180103159.jpg?1613384185', 1),
+	(180103159, 'widi_dwi@fikom.udb.ac.id', 'Widi Dwi', 6, 'Email: hmpti@gmail.com, Whatsapp: 08XXXXXXXX', 'Aku seorang pelaut. Aku suka memancing ikan cupang.', '180103159.jpg?1613384185', 1),
 	(190103155, 'a', 'Yahya', 8, '', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod\r\n        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,\r\n        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo\r\n        consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse\r\n        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non\r\n        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '190103155.jpg?1613384119', 1);
 /*!40000 ALTER TABLE `h_member` ENABLE KEYS */;
+
+-- Dumping structure for table db_hmpti.h_member_login_log
+DROP TABLE IF EXISTS `h_member_login_log`;
+CREATE TABLE IF NOT EXISTS `h_member_login_log` (
+  `id` int(255) NOT NULL AUTO_INCREMENT,
+  `email` varchar(255) NOT NULL DEFAULT '0',
+  `time` int(20) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table db_hmpti.h_member_login_log: ~1 rows (approximately)
+DELETE FROM `h_member_login_log`;
+/*!40000 ALTER TABLE `h_member_login_log` DISABLE KEYS */;
+INSERT INTO `h_member_login_log` (`id`, `email`, `time`) VALUES
+	(1, 'widi_dwi@fikom.udb.ac.id', 1613883589),
+	(2, 'widi_dwi@fikom.udb.ac.id', 1613889760);
+/*!40000 ALTER TABLE `h_member_login_log` ENABLE KEYS */;
+
+-- Dumping structure for table db_hmpti.h_panitia
+DROP TABLE IF EXISTS `h_panitia`;
+CREATE TABLE IF NOT EXISTS `h_panitia` (
+  `id_panitia` int(11) NOT NULL AUTO_INCREMENT,
+  `nim` int(12) NOT NULL DEFAULT 0,
+  `id_event` int(12) NOT NULL DEFAULT 0,
+  `peran` varchar(255) NOT NULL DEFAULT '0',
+  PRIMARY KEY (`id_panitia`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table db_hmpti.h_panitia: ~0 rows (approximately)
+DELETE FROM `h_panitia`;
+/*!40000 ALTER TABLE `h_panitia` DISABLE KEYS */;
+/*!40000 ALTER TABLE `h_panitia` ENABLE KEYS */;
+
+-- Dumping structure for table db_hmpti.h_pendaftar
+DROP TABLE IF EXISTS `h_pendaftar`;
+CREATE TABLE IF NOT EXISTS `h_pendaftar` (
+  `id_pendaftar` int(11) NOT NULL AUTO_INCREMENT,
+  `email` varchar(500) DEFAULT '',
+  `id_event` int(12) NOT NULL DEFAULT 0,
+  `bintang` tinyint(1) NOT NULL DEFAULT 0,
+  PRIMARY KEY (`id_pendaftar`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- Dumping data for table db_hmpti.h_pendaftar: ~0 rows (approximately)
+DELETE FROM `h_pendaftar`;
+/*!40000 ALTER TABLE `h_pendaftar` DISABLE KEYS */;
+/*!40000 ALTER TABLE `h_pendaftar` ENABLE KEYS */;
 
 -- Dumping structure for table db_hmpti.h_proker
 DROP TABLE IF EXISTS `h_proker`;
@@ -160,14 +209,14 @@ CREATE TABLE IF NOT EXISTS `h_proker` (
   `deskripsi` text DEFAULT '',
   `id_divisi` int(11) DEFAULT NULL,
   PRIMARY KEY (`id_proker`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4;
 
 -- Dumping data for table db_hmpti.h_proker: ~2 rows (approximately)
 DELETE FROM `h_proker`;
 /*!40000 ALTER TABLE `h_proker` DISABLE KEYS */;
 INSERT INTO `h_proker` (`id_proker`, `judul`, `waktu`, `deskripsi`, `id_divisi`) VALUES
-	(1, 'Website HMPTI', 'Februari 2021', 'Deskripsi Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 4),
-	(2, 'Makrab', 'Februari 2021', 'MakrabLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 1);
+	(2, 'Makrabs', 'Februari 2021', 'MakrabLorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua', 1),
+	(4, 'Choku City', 'Desembar 2022', 'Membuat sesuatu menjadi sesuatu sehingga sesuatu maka sesuatu ini menjadi sesuatu.', 1);
 /*!40000 ALTER TABLE `h_proker` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
