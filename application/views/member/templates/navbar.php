4f50a3f3196f35e3c765c9ef8fefb002231da2cc
widibaka <widi_dwi@fikom.udb.ac.id>
2021-09-13 22:14:29 +0700

    finished

--- application/views/member/templates/navbar.php
+++ application/views/member/templates/navbar.php
@@ -6,7 +6,7 @@
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Ver. 1.1</a>
+        <a href="#" class="nav-link"></a>
       </li>
       <!-- 
       <li class="nav-item d-none d-sm-inline-block">
